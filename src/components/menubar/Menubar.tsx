b014c9ba248103252a00a465aa094978b041bda6
minkyeong <ming920331@gmail.com>
2025-05-18 13:27:14 +0900

    feature: [fe] menubar overlay 추가 및 scroll 방지

--- src/components/menubar/Menubar.tsx
+++ src/components/menubar/Menubar.tsx
@@ -2,12 +2,8 @@
 
 import useMenuStore from "@/store/useMenuStore";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { LuCircleX } from "react-icons/lu";
-
-// interface MenubarProps{
-//     onClose: () => void,
-//     isOpen: boolean
-// }
 
 function Menubar() {
     const router = useRouter();
@@ -17,106 +13,84 @@ function Menubar() {
         closeMenu();
         router.push("/");
     }
+
+    useEffect(() => {
+        if(isMenuOpen){
+            document.body.style.overflow = "hidden";
+        }else {
+            document.body.style.overflow = "";
+        }
+    }, [isMenuOpen]);
+
+    if(!isMenuOpen) return null;
     
     return (
-        <div className={`bg-white
-                        w-[20vw]
-                        z-15
-                        fixed
-                        top-0 
-                        left-0
-                        h-screen
-                        rounded-tr-3xl
-                        rounded-br-3xl
-                        p-8
-                        transition-all
-                        duration-300
-                        transform
-                        ${isMenuOpen ? 'translate-x-0' : '-translate-x-full invisible'}
-        `}>
-            <div className="flex
-                            flex-row
-                            justify-between
-                            items-center
-                            text-3xl
-                            font-bold
+        <>
+            {/** Over Lay */}
+            <div className="fixed inset-0 h-screen bg-gray-500 opacity-50 z-10"
+                onClick={closeMenu} 
+            />
+
+            {/** Menu Bar */}
+            <div className="bg-white
+                            w-[20vw]
+                            z-15
+                            fixed
+                            top-0 
+                            left-0
+                            h-screen
+                            rounded-tr-3xl
+                            rounded-br-3xl
+                            p-8
+                            transition-all
+                            duration-300
+                            transform
+                            translate-x-0
             ">
-                <div>Sign in</div>
-                <LuCircleX onClick={closeMenu} className="cursor-pointer" />
-            </div>
-            <div className="border border-gray-200 my-7"></div>
-            <nav className="font-extrabold
-                            text-5xl 
-            ">
-                <ul className="flex
-                                flex-col
-                                gap-y-15
+            {/* <div className={`bg-white
+                            w-[20vw]
+                            z-15
+                            fixed
+                            top-0 
+                            left-0
+                            h-screen
+                            rounded-tr-3xl
+                            rounded-br-3xl
+                            p-8
+                            transition-all
+                            duration-300
+                            transform
+                            ${isMenuOpen ? 'translate-x-0' : '-translate-x-full invisible'}
+            `}> */}
+                <div className="flex
+                                flex-row
+                                justify-between
+                                items-center
+                                text-3xl
+                                font-bold
                 ">
-                    <li className="cursor-pointer" onClick={goHome}>Home</li>
-                    <li className="cursor-pointer" onClick={() => {}}>Order</li>
-                    <li className="cursor-pointer" onClick={() => {}}>Locations</li>
-                    <li className="cursor-pointer" onClick={() => {}}>Catering</li>
-                    <li className="cursor-pointer" onClick={() => {}}>Gift Card</li>
-                    <li className="cursor-pointer" onClick={() => {}}>Merch</li>
-                </ul>
-            </nav>
-        </div>
+                    <div>Sign in</div>
+                    <LuCircleX onClick={closeMenu} className="cursor-pointer" />
+                </div>
+                <div className="border border-gray-200 my-7"></div>
+                <nav className="font-extrabold
+                                text-5xl 
+                ">
+                    <ul className="flex
+                                    flex-col
+                                    gap-y-15
+                    ">
+                        <li className="cursor-pointer" onClick={goHome}>Home</li>
+                        <li className="cursor-pointer" onClick={() => {}}>Order</li>
+                        <li className="cursor-pointer" onClick={() => {}}>Locations</li>
+                        <li className="cursor-pointer" onClick={() => {}}>Catering</li>
+                        <li className="cursor-pointer" onClick={() => {}}>Gift Card</li>
+                        <li className="cursor-pointer" onClick={() => {}}>Merch</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     );
 }
-
-// function Menubar({isOpen, onClose} : MenubarProps) {
-//     const router = useRouter();
-//     const {isOpen, open, close} = useMenuStore();
-
-//     const goHome = () => {
-//         onClose();
-//         router.push("/");
-//     }
-    
-//     return (
-//         <div className={`bg-white
-//                         w-[20vw]
-//                         z-15
-//                         fixed
-//                         top-0 
-//                         left-0
-//                         h-screen
-//                         rounded-tr-3xl
-//                         rounded-br-3xl
-//                         p-8
-//                         transition-all
-//                         duration-300
-//                         transform
-//                         ${isOpen ? 'translate-x-0' : '-translate-x-full invisible'}
-//         `}>
-//             <div className="flex
-//                             flex-row
-//                             justify-between
-//                             items-center
-//                             text-3xl
-//                             font-bold
-//             ">
-//                 <div>Sign in</div>
-//                 <LuCircleX onClick={onClose} className="cursor-pointer" />
-//             </div>
-//             <div className="border border-gray-200 my-7"></div>
-//             <nav className="font-extrabold
-//                             text-5xl 
-//             ">
-//                 <ul className="flex
-//                                 flex-col
-//                                 gap-y-15
-//                 ">
-//                     <li className="cursor-pointer" onClick={goHome}>Home</li>
-//                     <li className="cursor-pointer" onClick={() => {}}>Order</li>
-//                     <li className="cursor-pointer" onClick={() => {}}>Locations</li>
-//                     <li className="cursor-pointer" onClick={() => {}}>Catering</li>
-//                     <li className="cursor-pointer" onClick={() => {}}>Gift Card</li>
-//                     <li className="cursor-pointer" onClick={() => {}}>Merch</li>
-//                 </ul>
-//             </nav>
-//         </div>
-//     );
-// }
 
 export default Menubar;
